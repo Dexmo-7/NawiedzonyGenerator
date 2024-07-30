d533e21faaa83246ac4089b35d5bb3c561ed2ce2
Dexmo-7 <mowsianka13@gmail.com>
2024-07-30 15:55:11 +0200

    Modify draft file. Add background to the window

--- draft.py
+++ draft.py
@@ -1,11 +1,20 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QtWidget
+from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5.QtGui import QPixmap
 
-class AppDemo(QtWidget):
+class AppDemo(QDialog):
     def __init__(self):
         super().__init__()
+
+        self.label = QLabel(self)
+        self.pixmap = QPixmap('background.png')
+        self.label.setPixmap(self.pixmap)
         uic.loadUi('draft.ui', self)
+        self.setWindowTitle("Generator")
+        self.setGeometry(0, 0, 600, 400)
+        self.label.resize(self.pixmap.width(), self.pixmap.height())
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
